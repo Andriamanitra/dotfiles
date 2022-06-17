3d9ec31d9f4eb434cf3ae72a7a1fcc68b17f11b6
Andriamanitra <mikko77792@gmail.com>
2022-06-18 02:05:25 +0300

    update search plugin, use kagi instead of google

--- terminator/.config/terminator/plugins/searchplugin.py
+++ terminator/.config/terminator/plugins/searchplugin.py
@@ -1,14 +1,18 @@
 import os
 import tempfile
+import webbrowser
 
 import terminatorlib.plugin as plugin
 
 import gi
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
-from gi.repository import Gdk
+from gi.repository import Gtk  # noqa: E402
+from gi.repository import Gdk  # noqa: E402
+
 
 AVAILABLE = ['SearchPlugin']
+SEARCH_URL = "https://kagi.com/search?q=%s"
+
 
 class SearchPlugin(plugin.MenuItem):
     capabilities = ['terminal_menu']
@@ -20,21 +24,29 @@ class SearchPlugin(plugin.MenuItem):
         clip = Gtk.Clipboard.get(Gdk.SELECTION_PRIMARY)
         self.selection = clip.wait_for_text().strip()
 
-        # if selection is long, give an option to open it in an editor,
-        # otherwise give an option to google it!
-        if len(self.selection) > 50 or "\n" in self.selection:
+        # if selection is long, give an option to open it in an editor
+        if len(self.selection) > 150 or "\n" in self.selection:
             item = Gtk.MenuItem("Open in editor")
             item.connect("activate", self.editor)
             menu.insert(item, 2)
-        else:
+
+        # if selection is short enough, give an option to search for it
+        if len(self.selection) <= 200:
             item = Gtk.MenuItem("Search")
             item.connect("activate", self.search)
-            item.set_label(f"Search '{self.selection}'")
+            search_text = self.cleaned_selection
+            if len(search_text) > 50:
+                search_text = search_text[:48] + "..."
+            item.set_label(f"Search '{search_text}'")
             menu.insert(item, 2)
 
+    @property
+    def cleaned_selection(self):
+        """Returns selection without extra whitespace"""
+        return " ".join(self.selection.split())
+
     def search(self, menu):
-        url = "https://www.google.com/search?q=" + self.selection
-        os.system(f"xdg-open '{url}'")
+        webbrowser.open(SEARCH_URL % self.cleaned_selection)
 
     def editor(self, menu):
         tempf = tempfile.NamedTemporaryFile(mode="w+t", delete=False)
@@ -42,4 +54,3 @@ class SearchPlugin(plugin.MenuItem):
         n = tempf.name
         tempf.close()
         os.system(f"xdg-open {n}")
-
