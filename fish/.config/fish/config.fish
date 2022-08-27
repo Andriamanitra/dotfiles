4c449fc62c95cedb8bb0a929f54914bd83263bb8
Andriamanitra <mikko77792@gmail.com>
2022-08-27 23:43:58 +0300

    add some handy shortcuts like !!

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -42,3 +42,41 @@ type -q npm && npm config set prefix ~/Packages/npm
 
 # enable fzf key bindings
 type -q fzf && fzf_key_bindings
+
+# key bindings:
+# !! = last command from history
+# head! = first token of the last command from history
+# last! = last token of the last command from history
+# init! = all but last token of the last commmand from history
+# tail! = all but first token of the last command from history
+function bind_bang --description "shortcuts using !"
+    set tok (commandline --current-token)[-1]
+    switch $tok
+        case "!"
+            commandline --current-token -- $history[1]
+            commandline --function repaint
+        case "first" "head"
+            echo $history[1] | read --local --list --tokenize last_cmd
+            commandline --current-token -- $last_cmd[1]
+            commandline --function repaint
+        case "last"
+            echo $history[1] | read --local --list --tokenize last_cmd
+            commandline --current-token -- $last_cmd[-1]
+            commandline --function repaint
+        case "init"
+            echo $history[1] | read --local --list --tokenize last_cmd
+            commandline --current-token -- "$last_cmd[1..-2]"
+            commandline --function repaint
+        case "rest" "tail"
+            echo $history[1] | read --local --list --tokenize last_cmd
+            commandline --current-token -- "$last_cmd[2..-1]"
+            commandline --function repaint
+        case "*"
+            commandline --insert !
+    end
+end
+
+
+function fish_user_key_bindings
+    bind ! bind_bang
+end
