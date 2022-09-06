30691dcb7e23961cfb23d82e0fb2cf6301736101
Andriamanitra <mikko77792@gmail.com>
2022-09-06 12:10:01 +0300

    move one-time setup to a function to significantly speed up fish start up

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,48 +1,3 @@
-# Disable default greeting ("Welcome to fish, the friendly interactive shell")
-set fish_greeting
-
-# Don't ask which man page if there are multiple matches,
-# just pick the first matching one
-set -Ux MAN_POSIXLY_CORRECT
-
-fish_add_path ~/Packages/cargo/bin
-fish_add_path ~/Packages/npm/bin
-fish_add_path ~/.local/bin
-fish_add_path ~/bin
-
-# Make micro (https://github.com/zyedidia/micro/) the default editor if it is installed
-type -q micro && set -Ux EDITOR (which micro)
-
-# If bat (https://github.com/sharkdp/bat/) is installed
-if type -q bat
-
-    # Use bat as a manpager (for syntax highlighting in man pages)
-    set -Ux MANPAGER "sh -c \"col -bx | bat -l man -p\""
-    set -Ux MANROFFOPT "-c" # get rid of extra formatting characters
-
-    # replace "cat" with bat's plain mode (no decorations or paging)
-    # NOTE: use ctrl+space instead of space to prevent abbreviation from expanding
-    alias cat="bat -pp"
-end
-
-# If btop is installed
-if type -q btop
-    alias top="btop"
-end
-
-# Package installation locations
-# todo: maybe use .gemrc, .npmrc instead?
-set -Ux CARGO_PATH ~/Packages/cargo
-set -Ux GEM_HOME ~/Packages/gem
-set -Ux GEM_PATH $GEM_HOME:/usr/lib64/ruby/gems/3.0.0
-set -Ux GEM_SPEC_CACHE ~/Packages/gem/spec_cache
-set -Ux GOPATH ~/Packages/go
-set -Ux JULIA_DEPOT_PATH ~/Packages/julia
-type -q npm && npm config set prefix ~/Packages/npm
-
-# enable fzf key bindings
-type -q fzf && fzf_key_bindings
-
 # key bindings:
 # !! = last command from history
 # head! = first token of the last command from history
@@ -79,4 +34,6 @@ end
 
 function fish_user_key_bindings
     bind ! bind_bang
+    # enable fzf key bindings
+    fzf_key_bindings
 end
---
+++ fish/.config/fish/functions/fish_setup.fish
@@ -0,0 +1,47 @@
+function fish_setup --description "run once to setup env variables, aliases, etc"
+    # Disable default greeting ("Welcome to fish, the friendly interactive shell")
+    set fish_greeting
+
+    # Don't ask which man page if there are multiple matches,
+    # just pick the first matching one
+    set -Ux MAN_POSIXLY_CORRECT
+
+    fish_add_path ~/Packages/cargo/bin
+    fish_add_path ~/Packages/npm/bin
+    fish_add_path ~/.local/bin
+    fish_add_path ~/bin
+
+    # Make micro (https://github.com/zyedidia/micro/) the default editor if it is installed
+    type -q micro && set -Ux EDITOR (which micro)
+
+    # If bat (https://github.com/sharkdp/bat/) is installed
+    if type -q bat
+
+        # Use bat as a manpager (for syntax highlighting in man pages)
+        set -Ux MANPAGER "sh -c \"col -bx | bat -l man -p\""
+        set -Ux MANROFFOPT "-c" # get rid of extra formatting characters
+
+        # replace "cat" with bat's plain mode (no decorations or paging)
+        # NOTE: use ctrl+space instead of space to prevent abbreviation from expanding
+        alias --save cat="bat -pp"
+    else
+        functions -e cat
+    end
+
+    # If btop is installed
+    if type -q btop
+        alias --save top="btop"
+    else
+        functions -e top
+    end
+
+    # Package installation locations
+    # todo: maybe use .gemrc, .npmrc instead?
+    set -Ux CARGO_PATH ~/Packages/cargo
+    set -Ux GEM_HOME ~/Packages/gem
+    set -Ux GEM_PATH $GEM_HOME:/usr/lib64/ruby/gems/3.0.0
+    set -Ux GEM_SPEC_CACHE ~/Packages/gem/spec_cache
+    set -Ux GOPATH ~/Packages/go
+    set -Ux JULIA_DEPOT_PATH ~/Packages/julia
+    type -q npm && npm config set prefix ~/Packages/npm
+end
